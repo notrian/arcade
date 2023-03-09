2467d365e7a40f89976e1e934bf5cd0dc8238c64
rian <rnevcasey@gmail.com>
2023-03-09 12:06:12 -0500

    Added ticks to the game, functionally moving box, starting game ender page

--- js/snake.js
+++ js/snake.js
@@ -1,9 +1,77 @@
 
-const board = document.getElementById('game');
+let snake = {
+    body: [ [9, 9]],
+    nextDirection: [1, 0]
+}
 
-for (let i = 0; i < (17*17); i++) {
+let gameState = {
+    apple: [11, 8],
+    snake: snake // from above
+}
+
+let boardSize = {
+    width: 17,
+    height: 17
+}
+
+const board = document.getElementById('game');
+board.style.gridTemplateColumns = `repeat(${boardSize.width}, 1fr);`;
+
+for (let i = 0; i < (boardSize.width*boardSize.height); i++) {
     let newChild = document.createElement('div');
     newChild.className = 'game-cell';
     if (i % 2 !== 0) newChild.classList.add('every-other');
     board.appendChild(newChild);
+}
+
+
+let direction = '';
+document.addEventListener('keydown', (e) => {
+    key = e.key.toLowerCase();
+    if (key === 'w' || key === 'arrowup') direction = 'up';
+    if (key === 's' || key === 'arrowdown') direction = 'down';
+    if (key === 'a' || key === 'arrowleft') direction = 'left';
+    if (key === 'd' || key === 'arrowright') direction = 'right';
+    if (direction.length) e.preventDefault();
+})
+
+setInterval(() => {
+}, 100);
+tick();
+
+function tick() {
+    
+    // udpate moving pixels
+    if (direction === 'up') snake.body.forEach(bodyPart => { bodyPart[1]--; });
+    if (direction === 'down') snake.body.forEach(bodyPart => { bodyPart[1]++; });
+    if (direction === 'left') snake.body.forEach(bodyPart => { bodyPart[0]--; });
+    if (direction === 'right') snake.body.forEach(bodyPart => { bodyPart[0]++; });
+    
+
+
+    // show snake body
+    let row = 1, column = 1;
+    for (let i = 0; i < board.childNodes.length; i++) {
+        const cell = board.childNodes[i];
+        snake.body.forEach(bodyPart => {
+            if (column === bodyPart[0] + 1 && row === bodyPart[1]) {
+                cell.style.background = "white";
+            }
+            else {
+                if (!cell.style) return;
+                if (i % 2 === 0) cell.style.background = document.documentElement.style.getPropertyValue('--game-color-1');
+                else cell.style.background = document.documentElement.style.getPropertyValue('--game-color-2');
+            }
+        });
+
+        if (boardSize.width === column) {
+            column = 1;
+            row++;
+        } else column++;
+    }
+}
+endGame();
+function endGame() {
+    board.style.opacity = '0.3';
+    board.style.border = `2px solid ${document.documentElement.style.getPropertyValue('--game-color-2')}`;
 }
