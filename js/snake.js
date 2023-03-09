0663834f92d92bf2e28bdcba9a371e62ca9bb746
rian <rnevcasey@gmail.com>
2023-03-09 10:39:22 -0500

    Removed play and info pages (this will be a single page app) and created board, also started the process of being able to change colors

--- js/snake.js
+++ js/snake.js
@@ -1,1 +1,9 @@
 
+const board = document.getElementById('game');
+
+for (let i = 0; i < (17*17); i++) {
+    let newChild = document.createElement('div');
+    newChild.className = 'game-cell';
+    if (i % 2 !== 0) newChild.classList.add('every-other');
+    board.appendChild(newChild);
+}
