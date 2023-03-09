0f8f10e70c662559c8440997ddfb1602f0e0ba6f
rian <rnevcasey@gmail.com>
2023-03-09 11:15:00 -0500

    Aded board color changing functionallity

---
+++ js/style.js
@@ -0,0 +1,32 @@
+const theme = document.getElementById('theme');
+theme.value = 'black';
+
+theme.addEventListener('change', (e) => {
+    let color1 = theme.value;
+    let color2 = '#151515';
+
+    if (theme.value === 'black') color1 = '#0f0f0f';
+    if (theme.value === 'white') color2 = '#cccccc';
+    if (theme.value === 'red') {
+        color1 = '#B02A2A';
+        color2 = '#fc3c3c';
+    }
+    if (theme.value === 'orange') color2 = '#b27300';
+    if (theme.value === 'yellow') color2 = '#b2b200';
+    if (theme.value === 'green') {
+        color1 = '#04a071';
+        color2 = '#066448';
+    }
+    if (theme.value === 'blue') {;
+        color1 = '#294f86';
+        color2 = '#396DBA';
+    }
+    if (theme.value === 'purple') {;
+        color1 = '#796bb2';
+        color2 = '#ae9aff';
+    }
+    if (theme.value === 'brown') color2 = '#581616';
+    
+    document.documentElement.style.setProperty('--game-color-1', color1)
+    document.documentElement.style.setProperty('--game-color-2', color2)
+});
