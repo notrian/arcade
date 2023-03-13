d80ec53024da52eff026c142f6334ea894e56a90
rian <rnevcasey@gmail.com>
2023-03-13 12:31:19 -0400

    tried alot of things but cant get snake to work and got irritated, will come back to it tomorow

--- js/snake.js
+++ js/snake.js
@@ -9,14 +9,17 @@ let gameState = {
     snake: snake // from above
 }
 let boardSize = {
-    width: 17,
-    height: 17
+    width: 19,
+    height: 19
 }
 
 const board = document.getElementById('game');
 const play = document.getElementById('play');
 const deadbox = document.getElementById('deadbox');
-board.style.gridTemplateColumns = `repeat(${boardSize.width}, 1fr);`;
+
+setTimeout(() => {
+    board.style.gridTemplateColumns = `repeat(${boardSize.width}, 1fr)`;    
+}, 120);
 
 
 // create all cells according to size
@@ -35,26 +38,35 @@ play.addEventListener('click', () => {
 
 
 // change snake.direction when key down
+let isTurning = false;
 document.addEventListener('keydown', (e) => {
+    if (isTurning) return;
+    isTurning = true;
     key = e.key.toLowerCase();
-    if (key === 'w' || key === 'arrowup') snake.direction = 'up';
-    if (key === 's' || key === 'arrowdown') snake.direction = 'down';
-    if (key === 'a' || key === 'arrowleft') snake.direction = 'left';
-    if (key === 'd' || key === 'arrowright') snake.direction = 'right';
+    if (snake.direction !== 'down' && (key === 'w' || key === 'arrowup')) snake.direction = 'up';
+    if (snake.direction !== 'up' && (key === 's' || key === 'arrowdown')) snake.direction = 'down';
+    if (snake.direction !== 'right' && (key === 'a' || key === 'arrowleft')) snake.direction = 'left';
+    if (snake.direction !== 'left' && (key === 'd' || key === 'arrowright')) snake.direction = 'right';
+    if (key === ' ' || key === 'enter') startGame();
     if (snake.direction.length) e.preventDefault();
-})
+    isTurning = false;
+    tick();
+});
 
 
 
 endGame();
 
-setInterval(() => {
-    if (gameState.gameStarted) tick();
-}, 100);
+// setInterval(() => {
+//     if (gameState.gameStarted) tick();
+// }, 200);
 
 function startGame() {
+    for (let i = 0; i < board.childNodes.length; i++) { 
+        if (board.childNodes[i].style) board.childNodes[i].style.background = '';
+    }
     snake = {
-        body: [ [9, 9]],
+        body: [ [Math.round(boardSize.width / 2), Math.round(boardSize.height / 2)]],
         direction: ''
     }
     gameState.gameStarted = true;
@@ -65,6 +77,12 @@ function startGame() {
 }
 
 function endGame() {
+    // udpate moving pixels
+    snake.body.forEach(bodyPart => { 
+        let beforeCell = findCell(bodyPart);
+        if (beforeCell !== undefined) beforeCell.style.background = '';
+    });
+
     gameState.gameStarted = false;
     play.innerText = 'Play';
     deadbox.style.display = 'block';
@@ -74,40 +92,114 @@ function endGame() {
 
 function tick() {
     
-    // udpate moving pixels
-    snake.body.forEach(bodyPart => { 
-        if (snake.direction === 'up') bodyPart[1]--; 
-        if (snake.direction === 'down') bodyPart[1]++;
-        if (snake.direction === 'left') bodyPart[0]--;
-        if (snake.direction === 'right') bodyPart[0]++;
-
-        if (bodyPart[0] < 1 || bodyPart[0] - 3 > boardSize.width) endGame();
-        if (bodyPart[1] < 0 || bodyPart[1] > boardSize.height) endGame();
-    });
-
-    if (!gameState.gameStarted) return;
+    let isOverApple = false;
     
-    // show snake body
-    let row = 1, column = 1;
-    for (let i = 0; i < board.childNodes.length; i++) {
-        const cell = board.childNodes[i];
-        snake.body.forEach(bodyPart => {
-            if (column === bodyPart[0] + 1 && row === bodyPart[1]) {
-                cell.style.background = "white";
-            }
-            else {
-                if (!cell.style) return;
-                if (i % 2 === 0) cell.style.background = document.documentElement.style.getPropertyValue('--game-color-1');
-                else cell.style.background = document.documentElement.style.getPropertyValue('--game-color-2');
-            }
-        });
+    // let snakeTail = snake.body[snake.body.length - 1];
+    // if (snakeTail === snakeHead) snakeTail = undefined;
+    let snakeHead = snake.body[snake.body.length - 1];
 
-        if (boardSize.width === column) {
-            column = 1;
-            row++;
-        } else column++;
-        console.log(row)
-        console.log(column)
+    if (snake.direction === 'up') snakeHead[1]--; 
+    if (snake.direction === 'down') snakeHead[1]++;
+    if (snake.direction === 'left') snakeHead[0]--;
+    if (snake.direction === 'right') snakeHead[0]++;
+    
+    snake.body.push(snakeHead);
+    snake.body.shift()
+    
+    for (let i = 0; i < snake.body.length; i++) {
+        let snakePart = snake.body[i];
+        findCell(snakePart).style.background = 'white';
+    }   
 
-    }
+    // if (snake.body.length > 1) {
+    // }
+    
+    console.log(snake.body)
+
+        /////////////////////////////////////////////
+
+    // for (let i = 0; i < snake.body.length; i++) {
+    //     let snakePart = snake.body[i];
+        
+    //     findCell(snakePart).style.background = '';
+
+    //     if (i === 0) {
+    //         if (snakePart === gameState.apple) {
+    //             console.log('d')
+    //             snakeHead = snakePart;
+    //         }
+    //     }
+        
+    //     if (snake.direction === 'up') snakePart[1]--; 
+    //     if (snake.direction === 'down') snakePart[1]++;
+    //     if (snake.direction === 'left') snakePart[0]--;
+    //     if (snake.direction === 'right') snakePart[0]++;
+        
+    //     if (i !== 0 && snakeHead === snakePart && snake.body.length > 1) return endGame();
+        
+    //     findCell(snakePart).style.background = 'white';
+    // }   
+    
+    // if (snakeHead[1] < 1 || snakeHead[1] > boardSize.height) return endGame();
+    // if (snakeHead[0] < 1 || snakeHead[0] > boardSize.width) return endGame();
+    
+    
+    // if (snakeHead && snakeHead[0] === gameState.apple[0] && snakeHead[1] === gameState.apple[1]) {
+    //     let newSnakePart = gameState.apple;
+    //     if (snake.direction === 'up') newSnakePart[1]--; 
+    //     if (snake.direction === 'down') newSnakePart[1]++;
+    //     if (snake.direction === 'left') newSnakePart[0]--;
+    //     if (snake.direction === 'right') newSnakePart[0]++;
+    //     snake.body.push(newSnakePart);
+    //     gameState.apple = [2,2];
+    // }
+
+    // findCell(gameState.apple).style.background = 'red';
+    
+    // oldSnakeHead = snakeHead;
+    /////////////////////////////////////////////
+    
+    // udpate moving pixels
+    // snake.body.forEach(bodyPart => { 
+        
+    //     if (bodyPart !== gameState.apple) {
+    //         let beforeCell = findCell(bodyPart);
+    //         if (beforeCell !== undefined) beforeCell.style.background = ''
+    //     }
+
+        
+    //     if (snake.direction === 'up') bodyPart[1]--; 
+    //     if (snake.direction === 'down') bodyPart[1]++;
+    //     if (snake.direction === 'left') bodyPart[0]--;
+    //     if (snake.direction === 'right') bodyPart[0]++;
+
+    //     if (snake.body[0] === bodyPart && interations) endGame();
+    //     if (bodyPart[0] < 1 || bodyPart[0] > boardSize.width) endGame();
+    //     if (bodyPart[1] < 1 || bodyPart[1] > boardSize.height) endGame();
+
+    //     if (gameState.gameStarted) {
+    //         let childCell = findCell(bodyPart);
+    //         childCell.style.background = 'white';
+    //     }
+
+    //     interations++;
+    // });
+
+    
+
+    // if (isOverApple) {
+    //     snake.body.push(gameState.apple)
+    //     gameState.apple = [1, 1]
+    //     console.log(snake.body)
+    // }
+    // else {
+    //     findCell(gameState.apple).style.background = 'red';
+    // }
+}
+
+function findCell(bodyPart) {
+    let index = 0;
+    let column = bodyPart[0];
+    let row = bodyPart[1];
+    return board.childNodes[(row * boardSize.width - boardSize.width) + column]
 }
